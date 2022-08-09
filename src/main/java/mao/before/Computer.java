43c6f504cff854071ed9b770e271b487c1629515
mao <1296193245@qq.com>
2022-08-09 21:55:22 +0800

    java_design_patterns_dependency_inversion_principle

--- src/main/java/mao/before/Computer.java
+++ src/main/java/mao/before/Computer.java
@@ -13,17 +13,28 @@ package mao.before;
  * Description(描述)： 无
  */
 
+
 public class Computer
 {
     private IntelCpu intelCpu;
     private KingstonMemory kingstonMemory;
     private XiJieHardDisk xiJieHardDisk;
 
+    /**
+     * Instantiates a new Computer.
+     */
     public Computer()
     {
 
     }
 
+    /**
+     * Instantiates a new Computer.
+     *
+     * @param intelCpu       the intel cpu
+     * @param kingstonMemory the kingston memory
+     * @param xiJieHardDisk  the xi jie hard disk
+     */
     public Computer(IntelCpu intelCpu, KingstonMemory kingstonMemory, XiJieHardDisk xiJieHardDisk)
     {
         this.intelCpu = intelCpu;
@@ -31,36 +42,69 @@ public class Computer
         this.xiJieHardDisk = xiJieHardDisk;
     }
 
+    /**
+     * Gets intel cpu.
+     *
+     * @return the intel cpu
+     */
     public IntelCpu getIntelCpu()
     {
         return intelCpu;
     }
 
+    /**
+     * Sets intel cpu.
+     *
+     * @param intelCpu the intel cpu
+     */
     public void setIntelCpu(IntelCpu intelCpu)
     {
         this.intelCpu = intelCpu;
     }
 
+    /**
+     * Gets kingston memory.
+     *
+     * @return the kingston memory
+     */
     public KingstonMemory getKingstonMemory()
     {
         return kingstonMemory;
     }
 
+    /**
+     * Sets kingston memory.
+     *
+     * @param kingstonMemory the kingston memory
+     */
     public void setKingstonMemory(KingstonMemory kingstonMemory)
     {
         this.kingstonMemory = kingstonMemory;
     }
 
+    /**
+     * Gets xi jie hard disk.
+     *
+     * @return the xi jie hard disk
+     */
     public XiJieHardDisk getXiJieHardDisk()
     {
         return xiJieHardDisk;
     }
 
+    /**
+     * Sets xi jie hard disk.
+     *
+     * @param xiJieHardDisk the xi jie hard disk
+     */
     public void setXiJieHardDisk(XiJieHardDisk xiJieHardDisk)
     {
         this.xiJieHardDisk = xiJieHardDisk;
     }
 
+    /**
+     * Run.
+     */
     public void run()
     {
         System.out.println("计算机工作");
