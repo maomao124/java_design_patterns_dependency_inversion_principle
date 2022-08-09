33700a70926c177a3756064996be9de47096aace
mao <1296193245@qq.com>
2022-08-09 22:01:01 +0800

    java_design_patterns_dependency_inversion_principle

---
+++ src/main/java/mao/after/HardDisk.java
@@ -0,0 +1,31 @@
+package mao.after;
+
+/**
+ * Project name(项目名称)：java设计模式_依赖倒转原则
+ * Package(包名): mao.after
+ * Interface(接口名): HardDisk
+ * Author(作者）: mao
+ * Author QQ：1296193245
+ * GitHub：https://github.com/maomao124/
+ * Date(创建日期)： 2022/8/9
+ * Time(创建时间)： 21:57
+ * Version(版本): 1.0
+ * Description(描述)： 无
+ */
+
+public interface HardDisk
+{
+    /**
+     * Save.
+     *
+     * @param data the data
+     */
+    void save(String data);
+
+    /**
+     * Get string.
+     *
+     * @return the string
+     */
+    String get();
+}
